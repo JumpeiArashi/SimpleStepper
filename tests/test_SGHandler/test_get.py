1b8982cfac915b7a6bd787438c766d92f93cf467
ARASHI, Jumpei <jumpei.arashi@arashike.com>
2014-08-20 16:16:12 +0900

    Add AWS unauthorized test

---
+++ tests/test_SGHandler/test_get.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import httplib
+import json
+
+import nose.tools
+import tornado.testing
+import simple_stepper
+
+
+class TestCase(tornado.testing.AsyncHTTPTestCase):
+
+    def get_app(self):
+        return simple_stepper.Application()
+
+    def test_unauthorized(self):
+        """
+        This case when failed to AWS authentication.
+        """
+        self.http_client.fetch(self.get_url('/allowIPs'), self.stop)
+        response = self.wait()
+
+        flag = True
+        messages = list()
+
+        if response.code != httplib.BAD_REQUEST:
+            flag = False
+            messages.append('Status Code is not 400.')
+
+        if json.loads(response.body).get('status_code') != httplib.BAD_REQUEST:
+            flag = False
+            messages.append('Status Code in message body is not 400.')
+
+        nose.tools.ok_(
+            flag,
+            msg='\n'.join(messages)
+        )
