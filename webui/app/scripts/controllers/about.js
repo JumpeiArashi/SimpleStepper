e3d0b3e189fa26c1b5428413129f9e0e5c3cb9a7
ARASHI, Jumpei <jumpei.arashi@arashike.com>
2014-08-21 00:17:55 +0900

    Init webui

---
+++ webui/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name simpleStepperWebuiApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the simpleStepperWebuiApp
+ */
+angular.module('simpleStepperWebuiApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ webui/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name simpleStepperWebuiApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the simpleStepperWebuiApp
+ */
+angular.module('simpleStepperWebuiApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
