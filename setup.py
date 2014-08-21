d52f4a95589c3aa220d30182633183997bd37b1d
ARASHI, Jumpei <jumpei.arashi@arashike.com>
2014-08-21 22:12:11 +0900

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup, find_packages
+
+setup(
+    name='SimpleStepper',
+    version='0.1.0',
+    description='Simprify your access to AWS resource.',
+    author='ARASHI, Jumpei',
+    author_email='jumpei.arashi@arashike.com',
+    url='https://github.com/JumpeiArashi/SimpleStepper',
+    license='MIT',
+    install_requires=[
+        'boto',
+        'tornado',
+    ],
+    classifiers=[
+        'Programing Language :: Python :: 2.6',
+        'Programing Language :: Python :: 2.7',
+    ]
+)
