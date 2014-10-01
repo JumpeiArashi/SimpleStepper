d12aaac2f5e1719268a3463a42cde1f14ce26f13
ARASHI, Jumpei <jumpei.arashi@arashike.com>
2014-10-01 21:14:56 +0900

    Oops:scream_cat: forgot removing 'keep_security_group_elements'

--- config.py
+++ config.py
@@ -6,4 +6,3 @@ region_name = 'AWS_REGION_NAME'
 aws_access_key_id = 'YOUR_AWS_ACCESS_KEY_ID'
 aws_secret_access_key = 'YOUR_AWS_SECRET_ACCESS_KEY'
 target_security_group_ids = ['TARGET_SECURITY_GROUP_IDS']
-keep_security_group_elements = ['THIS_OPTION_NOT_USE']
